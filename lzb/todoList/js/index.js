e1e46c82fb1e18cbfafe91dd4aa8456eb1c9f64b
unknown <2408012284@qq.com>
2021-01-05 19:31:41 +0800

    add lzb

--- lzb/todoList/js/index.js
+++ lzb/todoList/js/index.js
@@ -1,17 +1,14 @@
 (function() {
   var initValue = ['this is init value 1', 'this is init value 2', 'this is init value 3']
-
-  var addNode = function() { 
+  var finishValue = ['this is finish value 1', 'this is finish value 2', 'this is finish value 3'] 
+  var varNode = function() {
     var node = $('.form-control').val()
     initValue.unshift(node)
     renderList(initValue)
-    console.log(initValue)
     $('.form-control').val('')
   }
 
   var addFinish = function(e) {
-    // var index = $('#processing .list-group-item')
-    console.log(e.target.value)
     var index = parseInt(e.target.value, 10)
     var newNode = $('#processing .list-group-item:eq(' + index + ')')
     var lzb = $('#processing .list-group-item:eq(' + index + ')').text();
@@ -19,51 +16,36 @@
     initValue.splice(index, 1)
     finishValue.splice(0,0,lzb)
     renderList(initValue)
-    renderClone(finishValue)
   }
-
+  var addProcessing = function(e) {
+    var index = parseInt(e.target.value, 10)
+    var yesNode = $('#finish .list-group-item:eq(' + index + ')')
+    var hhb = $('#finish .list-group-item:eq(' + index + ')').text();
+    $('#processing .list-group').prepend(yesNode)
+    finishValue.splice(index, 1)
+    initValue.splice(0,0,hhb)
+    renderList(initValue)
+}
   var renderList = function(initValue) {
     $('#processing .list-group').empty()
     for(let i=0; i<initValue.length; i++) {
-      var html = '<li class="list-group-item" value=' + i + '>' + initValue[i] +  '</li>' //'<li class="list-group-item" value=0>this is init value 1</li>
+      var html = '<li class="list-group-item" value=' + i + '>' + initValue[i] +  '</li>'
       $('#processing .list-group').append(html)
     }
+    $('#finish .list-group').empty()
+    for(let i=0; i<finishValue.length; i++){
+        var html = '<li class="list-group-item" value=' + i + '>' + finishValue[i] +  '</li>'
+        $('#finish .list-group').append(html)
+    }
   }
-
   renderList(initValue)
 
-  // 绑定add，添加输入值到processing
-  $('.btn').click( function() { addNode() } )
-  
-  // 点击processing item，该item添加至finish
+  $('.btn').click( function() { varNode() } )
+
   $('#processing .list-group').click( function(e) { addFinish(e) } )
 
 
-  var finishValue = ['this is finish value 1', 'this is finish value 2', 'this is finish value 3']  /* 创造数组 */
-  var Processing = function(e) {
-      console.log(e.target.value)
-      var index = parseInt(e.target.value, 10)
-      var yesNode = $('#finish .list-group-item:eq(' + index + ')')
-      var hreat = $('#finish .list-group-item:eq(' + index + ')').text();
-      $('#processing .list-group').prepend(yesNode)
-      finishValue.splice(index, 1) /* 删除finishvalue数组 */
-      initValue.splice(0,0,hreat)
-      renderClone(initValue)
-      renderClone(finishValue)
-  }
-
-  var renderClone = function(finishValue) {
-      $('#finish .list-group').empty()
-      for(let i=0; i<finishValue.length; i++){
-          var html = '<li class="list-group-item" value=' + i + '>' + finishValue[i] +  '</li>'
-          $('#finish .list-group').append(html)
-          
-      }
-  }
-  
-  renderClone(finishValue)
-
-  $('#finish .list-group').click( function(e) { Processing(e) } )
+  $('#finish .list-group').click( function(e) { addProcessing(e) } )
 })()
 
 
