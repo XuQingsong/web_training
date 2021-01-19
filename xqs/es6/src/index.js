7f2d48296f5b415e4fe2f10e5553849073912ab1
XuQingsong <117373903@qq.com>
2021-01-19 22:36:18 +0800

    es6

--- xqs/es6/src/index.js
+++ xqs/es6/src/index.js
@@ -33,62 +33,107 @@ import 'babel-polyfill'
 
 // Set和Map
 
-const s = new Set([2,2,4,2,3,5,2,'5', NaN, NaN])
-const array = [2,2,4,2,3,5,2,'5', NaN, NaN]
-console.log(s, s[2], array[2])
+// const s = new Set([2,2,4,2,3,5,2,'5', NaN, NaN])
+// const array = [2,2,4,2,3,5,2,'5', NaN, NaN]
+// console.log(s, s[2], array[2])
 
-// const s = new Set(a)
-// const result = Array.from(s)
-// console.log(result)
+// // const s = new Set(a)
+// // const result = Array.from(s)
+// // console.log(result)
 
-function dedupe(array) {
-  return Array.from(new Set(array));
-}
+// function dedupe(array) {
+//   return Array.from(new Set(array));
+// }
 
-const dedupeA = (arr) => (Array.from(new Set(arr)))
-const a = [2,2,4,2,3,5,2,'5', NaN, NaN]
-console.log(dedupeA(a))
+// const dedupeA = (arr) => (Array.from(new Set(arr)))
+// const a = [2,2,4,2,3,5,2,'5', NaN, NaN]
+// console.log(dedupeA(a))
 
-const ob = {
-  a: '2eq',
-  b: 'dasdas',
-  1: '12321',
-}
-console.log(ob)
+// const ob = {
+//   a: '2eq',
+//   b: 'dasdas',
+//   1: '12321',
+// }
+// console.log(ob)
 
-// Map
-const data = {};
-const element = document.getElementById('myDiv');
+// // Map
+// const data = {};
+// const element = document.getElementById('myDiv');
 
-data[element] = 'metadata';
-console.log(data)
+// data[element] = 'metadata';
+// console.log(data)
 
-const m = new Map()
-const element1 = document.getElementById('myDiv');
-m.set(element1, 'metadata')
-console.log(m)
+// const m = new Map()
+// const element1 = document.getElementById('myDiv');
+// m.set(element1, 'metadata')
+// console.log(m)
 
-const items = [
-  ['name', '张三'],
-  ['title', 'Author']
-];
+// const items = [
+//   ['name', '张三'],
+//   ['title', 'Author']
+// ];
 
-const map = new Map(items);
-console.log(map)
+// const map = new Map(items);
+// console.log(map)
 
-// generator
+// // generator
 
-function* helloWorldGenerator() {
-  console.log('aaa');
-  yield 'hello';
-  console.log('bbb');
-  yield 'world';
-  console.log('ccc');
-  return 'ending';
-}
+// function* helloWorldGenerator() {
+//   console.log('aaa');
+//   yield 'hello';
+//   console.log('bbb');
+//   yield 'world';
+//   console.log('ccc');
+//   return 'ending';
+// }
 
-var hw = helloWorldGenerator();
-console.log(hw.next())
+// var hw = helloWorldGenerator();
+// console.log(hw.next())
 // console.log(hw.next())
 // console.log(hw.next())
 // hw.next()
+
+// async await
+
+const getStockSymbol = function () {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest()
+    xhr.open('get', 'http://crm.xuziqiao.com/api/finance', true)
+    xhr.send({})
+    xhr.onreadystatechange = function () { 
+      if(xhr.readyState === 4) {
+        if(xhr.status === 200) {
+          resolve(JSON.parse(xhr.response))
+        } else {
+          reject(xhr.response)
+        }
+      }
+    }
+  })
+}
+
+const getStockPrice = function () {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest()
+    xhr.open('get', 'http://crm.xuziqiao.com/api/finance', true)
+    xhr.send({})
+    xhr.onreadystatechange = function () { 
+      if(xhr.readyState === 4) {
+        if(xhr.status === 200) {
+          resolve(JSON.parse(xhr.response))
+        } else {
+          reject(xhr.response)
+        }
+      }
+    }
+  })
+}
+
+async function getStockPriceByName(name) {
+  const symbol = await getStockSymbol(name);
+  // console.log(symbol)
+  const stockPrice = await getStockPrice(symbol);
+  return stockPrice;
+}
+
+getStockPriceByName('goog').then(function (reslut) { console.log(reslut) })
